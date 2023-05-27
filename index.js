9cb087766bf5dc0eda0752f1b41aa895f5312346
Bradford <bzlee@ucdavis.edu>
2023-05-26 23:31:33 -0700

    condensed files, generated example logos

--- index.js
+++ index.js
@@ -2,33 +2,6 @@
 const {Circle, Triangle, Square} = require('./lib/shapes.js');
 const inquirer = require("inquirer");
 const fs = require('fs');
-// const setShape = require('./lib/setShape.js')
-
-
-// const newShape = new shapes(
-//     'SVG',
-//     'Text color OR hexadecimal',
-//     'Circle, Triangle, or Square',
-//     'Shape color OR hexadecimal',
-// );
-
-// newShape.printMetaData();
-// shapes.run();
-
-// function setShape(response) {
-//     if (response.shape === 'Circle'){
-//         let userShape = new Circle (response.text, response.textColor, response.shapeColor);
-//         return userShape.render();
-//     };
-//     if (response.shape === 'Triangle'){
-//         let userShape = new Triangle (response.text, response.textColor, response.shapeColor);
-//         return userShape.render();
-//     };
-//     if (response.shape === 'Square'){
-//         let userShape = new Square (response.text, response.textColor, response.shapeColor);
-//         return userShape.render();
-//     };
-// };
 
 // Create an array of questions for user input
 function startPrompt() {
@@ -36,12 +9,12 @@ function startPrompt() {
         {
             type: 'input',
             name: 'text',
-            message: 'Enter the text of your Logo:'
+            message: 'Enter the text of your Logo (Must be 3 characters or less):'
         },
         {
             type: 'input',
             name: 'textColor',
-            message: 'What is your text color?(Enter color keyword OR hexadecimal number):'
+            message: 'Enter the desired text color (color keyword OR hexadecimal number):'
         },
         {
             type: 'list',
@@ -52,7 +25,7 @@ function startPrompt() {
         {
             type: 'input',
             name: 'shapeColor',
-            message: 'What is your shape color?(Enter color keyword OR hexadecimal number):'
+            message: 'Enter the desired shape color (color keyword OR hexadecimal number):'
         }
     ]).then((response) => {
         if (response.text.length > 3) {
@@ -61,13 +34,9 @@ function startPrompt() {
         } else {
             writeToFile(response)
         }
-
-         console.log(response)
-    
-
     }).catch((err) => console.log(err))
 
-    function setShape(response) {
+    function defineShape(response) {
         if (response.shape === 'Circle'){
             let userShape = new Circle (response.text, response.textColor, response.shapeColor);
             return userShape.render();
@@ -81,9 +50,9 @@ function startPrompt() {
             return userShape.render();
         };
     };
-    
+
     function writeToFile(response) {
-        const svg = setShape(response);
+        const svg = defineShape(response);
         console.log(svg);
         // add fs code here to save file
         fs.writeFile("logo.svg", svg, (error) => {
@@ -95,6 +64,5 @@ function startPrompt() {
         })
     };
 };
-
 
 startPrompt();
